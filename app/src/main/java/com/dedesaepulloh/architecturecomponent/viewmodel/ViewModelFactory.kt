c4f9935515985fd0c4613d7dd628fb1d0017c0ae
dedesaepulloh77@gmail.com <dedesaepulloh77@gmail.com>
2021-07-28 13:30:04 +0700

    add view model factory

---
+++ app/src/main/java/com/dedesaepulloh/architecturecomponent/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,41 @@
+package com.dedesaepulloh.architecturecomponent.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.dedesaepulloh.architecturecomponent.data.source.CatalogRepository
+import com.dedesaepulloh.architecturecomponent.di.Injection
+import com.dedesaepulloh.architecturecomponent.ui.detail.DetailViewModel
+import com.dedesaepulloh.architecturecomponent.ui.home.movies.MoviesViewModel
+import com.dedesaepulloh.architecturecomponent.ui.home.tvshow.TvShowViewModel
+
+class ViewModelFactory private constructor(private val mCatalogRepository: CatalogRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+
+        fun getInstance(): ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(Injection.provideCatalogRepository()).apply {
+                    instance = this
+                }
+            }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(DetailViewModel::class.java) -> {
+                DetailViewModel(mCatalogRepository) as T
+            }
+            modelClass.isAssignableFrom(MoviesViewModel::class.java) -> {
+                MoviesViewModel(mCatalogRepository) as T
+            }
+            modelClass.isAssignableFrom(TvShowViewModel::class.java) -> {
+                TvShowViewModel(mCatalogRepository) as T
+            }
+            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+
+}

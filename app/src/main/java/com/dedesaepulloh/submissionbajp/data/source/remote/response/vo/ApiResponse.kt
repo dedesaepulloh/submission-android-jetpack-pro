a5c4a6db95a863f11f2e7eb193b167688fc74db5
dedesaepulloh77@gmail.com <dedesaepulloh77@gmail.com>
2021-08-15 22:47:33 +0700

    add package vo

---
+++ app/src/main/java/com/dedesaepulloh/submissionbajp/data/source/remote/response/vo/ApiResponse.kt
@@ -0,0 +1,17 @@
+@file:Suppress("unused", "unused")
+
+package com.dedesaepulloh.submissionbajp.data.source.remote.response.vo
+
+class ApiResponse<T>(val status: StatusResponse, val body: T, val message: String?) {
+
+    companion object {
+        fun <T> success(body: T): ApiResponse<T> = ApiResponse(StatusResponse.SUCCESS, body, null)
+
+        fun <T> empty(msg: String, body: T): ApiResponse<T> =
+            ApiResponse(StatusResponse.EMPTY, body, msg)
+
+        fun <T> error(msg: String, body: T): ApiResponse<T> =
+            ApiResponse(StatusResponse.ERROR, body, msg)
+    }
+
+}
---
+++ app/src/main/java/com/dedesaepulloh/submissionbajp/data/source/remote/response/vo/StatusResponse.kt
@@ -0,0 +1,7 @@
+package com.dedesaepulloh.submissionbajp.data.source.remote.response.vo
+
+enum class StatusResponse {
+    SUCCESS,
+    EMPTY,
+    ERROR
+}

04096298b0e138bc68e749a0f0209643a07957ac
dedesaepulloh77@gmail.com <dedesaepulloh77@gmail.com>
2021-08-15 22:47:32 +0700

    add package trending

---
+++ app/src/main/java/com/dedesaepulloh/submissionbajp/data/source/remote/response/trending/TrendingResponse.kt
@@ -0,0 +1,28 @@
+package com.dedesaepulloh.submissionbajp.data.source.remote.response.trending
+
+import com.google.gson.annotations.SerializedName
+
+data class TrendingResponse(
+
+    @SerializedName("id")
+    var id: Int? = null,
+
+    @SerializedName("original_title")
+    val original_title: String? = null,
+
+    @SerializedName("overview")
+    val overview: String? = null,
+
+    @SerializedName("popularity")
+    val popularity: Double? = null,
+
+    @SerializedName("poster_path")
+    val poster_path: String? = null,
+
+    @SerializedName("release_date")
+    val release_date: String? = null,
+
+    @SerializedName("vote_average")
+    val vote_average: Double? = null
+
+)
